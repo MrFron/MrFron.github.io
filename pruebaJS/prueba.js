bd130da09e3be99c87e2ed3691182839a2d0cd94
MrFron <65178366+MrFron@users.noreply.github.com>
2020-11-16 12:35:17 +0100

    Add files via upload
    Miniprogramas javascript M06-CT2-P2.1 (part 1)

---
+++ pruebaJS/prueba.js
@@ -0,0 +1,5 @@
+var numero_1 = 3;
+var numero_2 = 1;
+var resultado = numero_1 + numero_2;
+	
+alert(resultado);
---
+++ pruebaJS/prueba2.js
@@ -0,0 +1,5 @@
+var nombre = "Fran";
+var apellido = "Soriano";
+var nombre_entero = nombre + " " + apellido;
+	
+alert(nombre_entero);
