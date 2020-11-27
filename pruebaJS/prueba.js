3691ce51592a41263d57dc8882dfa9d031d423e7
MrFron <fransorianoroman@gmail.com>
2020-11-27 14:37:35 +0100

    Añadidos al portafolio los ej. hasta la 4.1(parte 1) y eliminados los ej sueltos

--- pruebaJS/prueba.js
+++
@@ -1,5 +0,0 @@
-var numero_1 = 3;
-var numero_2 = 1;
-var resultado = numero_1 + numero_2;
-	
-alert(resultado);
--- pruebaJS/prueba2.js
+++
@@ -1,5 +0,0 @@
-var nombre = "Fran";
-var apellido = "Soriano";
-var nombre_entero = nombre + " " + apellido;
-	
-alert(nombre_entero);
